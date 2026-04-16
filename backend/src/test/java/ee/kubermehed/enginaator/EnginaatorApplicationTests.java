d64ab239dfd0e4c77678473f9965226e29c2aac3
Timofei Beresnjev <bertimblch@gmail.com>
2026-04-16 17:50:33 +0300

    Add initial backend files

---
+++ backend/src/test/java/ee/kubermehed/enginaator/EnginaatorApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.kubermehed.enginaator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnginaatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
